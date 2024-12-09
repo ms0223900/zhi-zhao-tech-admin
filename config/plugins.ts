ff1dc95d2bfb8340cf399b6f80218533620218fb
penguin-cho <penguin-cho@cloud-latitude.com>
2024-12-09 21:05:11 +0800

    Add AWS S3 upload provider configuration and update dependencies

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+                region: env('AWS_REGION'),
+                params: {
+                    ACL: env('AWS_ACL', 'private'),
+                    signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                    Bucket: env('AWS_S3_BUCKET'),
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+});
